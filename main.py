b8120cf8a6e227c320d3d5a6aaf1cc64a0a46e35
Christian Adib <adib.christian@bcg.com>
2020-12-22 18:23:00 -0500

    renamed n_sim to n_iter and modified print statement

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import yaml
 if __name__ == "__main__":
 
     config = yaml.safe_load(open("config.yml"))
-    n_sim = config["n_iter"]
+    n_iter = config["n_iter"]
     mc = MonteCarlo(n_iter)
     mc.run_simulation()
-    print("{:.4%}".format((mc.p2_win - mc.p1_win) / (mc.p1_win + mc.p2_win)))
+    print("Second player advantage is: {:.4%}".format((mc.p2_win - mc.p1_win) / (mc.p1_win + mc.p2_win)))
