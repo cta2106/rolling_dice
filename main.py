efbc4f0b276e136fe245c38bc0510837c7789388
Christian Adib <adib.christian@bcg.com>
2020-12-22 18:24:19 -0500

    removed unnecessary variable

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import yaml
 if __name__ == "__main__":
 
     config = yaml.safe_load(open("config.yml"))
-    n_iter = config["n_iter"]
-    mc = MonteCarlo(n_iter)
+    mc = MonteCarlo(config["n_iter"])
     mc.run_simulation()
     print("Second player advantage is: {:.4%}".format((mc.p2_win - mc.p1_win) / (mc.p1_win + mc.p2_win)))
